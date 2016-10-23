eaea1ec256463b2789040a993ff0ca84e7667296
Marcel Fraas <mfraas64@gmail.com>
2016-10-23 19:31:49 +0200

    Setup Script added

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# install necessery tools
+yum install -y gcc-c++ patch \
+      readline readline-devel \
+      zlib zlib-devel libyaml-devel \
+      libffi-devel openssl-devel make \
+      bzip2 autoconf automake libtool \
+      bison iconv-devel libyaml \
+      rubygems ruby-devel git svn \
+      ntp ntpdate ntp-doc
+
+
+# add the hostname to /etc/hosts
+echo 127.0.0.1 $HOSTNAME >> /etc/hosts
+
+
+# enable ntpd
+chkconfig ntpd on
+ntpdate pool.ntp.org
+systemctl start ntpd
+
+
+# install pip
+cd /tmp
+curl -O https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install requests
+
+
+# install Ruby + Chef
+cd /tmp
+curl -O ftp://ftp.ruby-lang.org/pub/ruby/2.3/ruby-2.3.1.tar.gz
+tar -zxvf ruby-2.3.1.tar.gz
+cd ruby-2.3.1
+./configure --prefix=/usr/local
+make
+make install
+gem install chef ruby-shadow --no-ri --no-rdoc
+
+
+# install chef-librarian + dependencies
+mkdir /var/chef
+cd /var/chef
+gem install librarian-chef --no-ri --no-rdoc
+librarian-chef init
